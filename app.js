2524ee681c44f6323137cc85538b2854cea104b8
Furkan <gologlufurkan484@gmail.com>
2020-06-25 11:30:20 +0300

    prriv

--- app.js
+++ app.js
@@ -1,39 +1,125 @@
-const io = require('socket.io')(3000)
+const render = (response, path, data = {}) => require('ejs').renderFile(`./Views/${path}`, data)
+	.then(html => response.send(html))
+const express = require('express');
+const path = require('path');
+let app = express();
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
-let messages = [];
+app.use('/static', express.static("static"))
 
-const MAX_MESSAGE_COUNT = 100;
+const users = [{
+	id: "hX9hgfvZS9JHGutER785gCF57",
+	name: "Yasin"
+}, {
+	id: "JKvffgyDFgFXSYRKbH6s5rUfga",
+	name: "Furkan"
+}, {
+	id: "denemeid",
+	name: "test"
+}]
 
-const users = [
-	{
-		id : "hX9hgfvZS9JHGutER785gCF57",
-		name : "Yasin"
-	},
-	{
-		id : "JKvffgyDFgFXSYRKbH6s5rUfga", 
-		name : "Furkan"
-	}
-] 
+
+let online = 0;
 
 const getNameById = id => users.find(user => user.id == id).name;
+let messages = [];
 
-io.on('connection' , socket => {
+setInterval(() => {
+	messages = [];
+}, 1000 * 60 * 60 * 6)
 
-	socket.on("get-name" , data => {
-		console.log(data.ID)
-		socket.emit('your-name' , users.find(user => user.id == data.ID).name)
+const updateOnline = on => {
+	let onlineUsers = [];
+	for (let i = 0; i < users.length; i++) {
+		onlineUsers.push(items...: any)({
+			name: users[i].name,
+			online: users[i].online ? true : false
+		})
+	}
+	io.emit('online', {
+		online,
+		onlineUsers
+	})
+}
+
+io.on('connection', socket => {
+
+	online++;
+
+	users.forEach(function(user, index) {
+		if (user.id == socket.handshake.query.id) {
+			users[index].online = true;
+			users[index].sid = socket.id;
+		}
+	});
+
+	updateOnline();
+
+
+	socket.on('msg', data => {
+		if (isNaN(data.msg)) {
+			messages.push({
+				name: users.find(user => user.id == data.id).name,
+				msg: data.msg,
+				id: data.id
+			});
+			io.emit('msg', {
+				name: users.find(user => user.id == data.id).name,
+				msg: data.msg,
+				id: socket.id
+			});
+		}
 	})
 
-	socket.on('msg' , data => {
 
-		messages.push({
-			name : users.find(user => user.id == data.id).name,
-			msg : data.msg
-		})
 
-		io.emit('msg' , messages );
-
+	socket.on('disconnect', () => {
+		online--;
+		users.forEach((user, index) => {
+			if (user.sid == socket.id) {
+				user.sid = undefined;
+				user.online = false;
+				return;
+			}
+		});
+		updateOnline();
 	})
 })
 
+// setInterval(() => {console.log(online.length)} , 1000)
 
+
+app.get('/', (req, res) => {
+	let id = req.query.id;
+	let name = getNameById(id);
+	let initialMessages = messages;
+	render(res, "index.ejs", {
+		name,
+		id,
+		initialMessages,
+		users,
+		online
+	})
+})
+
+app.get('/del', (req, res) => {
+	messages = []
+	res.end();
+});
+
+app.get('/msgs', (req, res) => res.json(messages));
+
+app.get('/users', (req, res, next) => {
+	if (req.query.p == "allow") {
+		res.json(users)
+	} else next();
+})
+
+// app.get("/*" , (req , res) => {
+// 	res.status(404);
+// 	res.send("NOT FOUND");
+// })
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT);
